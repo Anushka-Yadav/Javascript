55a2a80b2481fd6dc5f6db929bb32ca6f170de8c
Anushka-Yadav <lncbbtcse026@lnctu.ac.in>
2023-12-28 22:35:16 +0530

    variable study

---
+++ variables.js
@@ -0,0 +1,7 @@
+let a = 90; //most suitable
+var b='anushka';  //not flexible;
+v="anu"          //not flexible
+console.log(a)
+console.log(b)
+console.log(v)
+console.table([a,b,v])  // in table form with list indexing
